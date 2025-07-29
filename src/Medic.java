7d3ecfbb84bd288be7f8d627190639058db3a5dc
TElzada <turdubekovaelzada@gmail.com>
2025-07-29 23:02:21 +0600

    completed Medic java class

--- src/Medic.java
+++ src/Medic.java
@@ -1,2 +1,18 @@
-public class Medic {
+public class Medic extends Hero {
+    private int healPoints;
+
+    public Medic(int health, int damage, String superAbilityType, int healPoints) {
+        super(health, damage, superAbilityType);
+        this.healPoints = healPoints;
+    }
+
+    public void increaseExperience() {
+        healPoints += healPoints * 0.1;
+        System.out.println("Опыт медика был увеличен. Теперь healPoints = " + healPoints);
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic применил супер способность " + getSuperAbilityType());
+    }
 }
