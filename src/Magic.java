f27820886e8bcaea8e206318cff3faf0e4542f71
TElzada <turdubekovaelzada@gmail.com>
2025-07-29 22:59:40 +0600

    completed Magic and Warrior java classes

--- src/Magic.java
+++ src/Magic.java
@@ -1,2 +1,11 @@
-public class Magic {
+public class Magic extends Hero {
+    public Magic(int health, int damage, String superAbilityType) {
+        super(health, damage, superAbilityType);
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Magic применил супер способность " + getSuperAbilityType());
+    }
 }
+
