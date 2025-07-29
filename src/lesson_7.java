02e565de7343f74820fdcf006e3c1a942f6797a1
TElzada <turdubekovaelzada@gmail.com>
2025-07-29 23:11:15 +0600

    finished the main class

--- src/lesson_7.java
+++ src/lesson_7.java
@@ -1,3 +1,17 @@
 public class lesson_7 {
+    public static void main(String[] args) {
+        Hero[] heroes = {
+                new Magic(200, 50, "FIREBALL"),
+                new Medic(150, 40, "HEALING", 50),
+                new Warrior(100, 20, "CRITICAL DAMAGE")
+        };
 
+        for (Hero hero : heroes) {
+            hero.applySuperAbility();
+            if (hero instanceof Medic) {
+                Medic medic = (Medic) hero;
+                medic.increaseExperience();
+            }
+        }
+    }
 }
